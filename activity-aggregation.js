e45e269f2abae0a1d4f4acc6e83b48b269cc884c
pknn <106571647+paulkennethheyrana@users.noreply.github.com>
2022-10-23 22:33:34 +0800

    Add activity code

---
+++ activity-aggregation.js
@@ -0,0 +1,87 @@
+db.Employee.insertMany([
+
+/* 1 */
+{
+    "_id": 312456,
+    "EmployeeName": "Joongki Song",
+    "EmployeeAge": 28,
+    "JobRole": "Store Manager",
+    "salary": 120000
+}
+/* 2 */
+{
+    "_id": 345342,
+    "EmployeeName": "Joongkuk jeon",
+    "EmployeeAge": 25,
+    "JobRole": "Store Associate",
+    "salary": 45000
+}
+/* 3 */
+{
+    "_id": 334566,
+    "EmployeeName": "Jin Kim",
+    "EmployeeAge": 35,
+    "JobRole": "Cashier",
+    "salary": 67500
+}
+/* 4 */
+{
+    "_id": 245345,
+    "EmployeeName": "Hyekyo Song",
+    "EmployeeAge": 40,
+    "JobRole": "Senior Cashier",
+    "salary": 72500
+}
+/* 5 */
+{
+    "_id": 445364,
+    "EmployeeName": "Tae Hyung Kim",
+    "EmployeeAge": 22,
+    "JobRole": "Store Associate",
+    "salary": 35000
+}
+]);
+
+db.Inventory.insertMany([
+/* 1 */
+{
+    "_id": 312456,
+    "EmployeeName": "Joongki Song",
+    "EmployeeAge": 28,
+    "JobRole": "Store Manager",
+    "salary": 120000
+}
+/* 2 */
+{
+    "_id": 345342,
+    "EmployeeName": "Joongkuk jeon",
+    "EmployeeAge": 25,
+    "JobRole": "Store Associate",
+    "salary": 45000
+}
+/* 3 */
+{
+    "_id": 334566,
+    "EmployeeName": "Jin Kim",
+    "EmployeeAge": 35,
+    "JobRole": "Cashier",
+    "salary": 67500
+}
+/* 4 */
+{
+    "_id": 245345,
+    "EmployeeName": "Hyekyo Song",
+    "EmployeeAge": 40,
+    "JobRole": "Senior Cashier",
+    "salary": 72500
+}
+/* 5 */
+{
+    "_id": 445364,
+    "EmployeeName": "Tae Hyung Kim",
+    "EmployeeAge": 22,
+    "JobRole": "Store Associate",
+    "salary": 35000
+}
+]);
+
